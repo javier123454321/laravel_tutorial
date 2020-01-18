81f2ef135e530d03fda327fed32eaa6c21a491e2
Javier Gonzalez <Hal@Dr-Jeckyll.lan>
2020-01-17 21:48:25 -0600

    stye

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,8 +24,13 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                <div><img src="/svg/freeCodeGram.svg" alt="logo"></img> </div>
+                <div>
+                    <img src="/svg/freeCodeGram.svg" alt="logo" class="logo pr-3"></img>
+                </div>
+                <div>
                     fc_gram
+                </div>
+                    
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
